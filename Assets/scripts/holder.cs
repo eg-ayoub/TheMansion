f1db560e5f8692e16a9bc22bb5bd394092b92239
ayoubx <ayoub.nasr.work@gmail.com>
2018-09-16 15:39:03 +0200

    added reset timer to levels

---
+++ Assets/scripts/holder.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holder : MonoBehaviour {
+
+	Animator anim;
+	enum states {IDLE, COUNT, RESET};
+	states state;
+	bool buttonUp, buttonDown;
+	int counter;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		state = states.IDLE;
+		counter = 0;
+	}
+	
+	void Update () {
+		buttonDown = Input.GetButtonDown("back");
+		buttonUp = Input.GetButtonUp("back");
+
+		switch(state){
+			case states.IDLE:
+				if(buttonDown){
+					counter = 0;
+					state = states.COUNT;
+					anim.SetFloat("speed", 1);
+				}
+				break;
+
+			case states.COUNT:
+				if(buttonUp){
+					anim.SetFloat("speed", -1);
+					state = states.RESET;
+				}else{
+					counter++;
+					if(counter == 120){
+						state = states.IDLE;
+						anim.Play("load", -1, 0f);
+						anim.SetFloat("speed", 0);
+						PlayerInstanciationScript.Player.GetComponentInChildren<PlayerHealthManager>().SendMessage("TakeDamage", 1);
+					}
+				}
+				break;
+
+			case states.RESET:
+				if(buttonDown){
+					anim.SetFloat("speed", 1);
+					state = states.COUNT;
+				}else{
+					counter--;
+					if(counter == 0){
+						state = states.IDLE;
+						anim.Play("load", -1, 0f);
+						anim.SetFloat("speed", 0);
+					}
+				}
+				break;
+
+		}
+	}
+}
